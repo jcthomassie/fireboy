264b7bbccd3985d7012a444ec56326e245a46ac5
Julian Thomassie <julianthomassie@gmail.com>
2022-01-23 15:36:39 -0800

    Echo back user message

--- main.go
+++ main.go
@@ -3,32 +3,72 @@ package main
 import (
 	"flag"
 	"fmt"
-	"github.com/bwmarrin/discordgo"
 	"os"
+	"os/signal"
+	"strings"
+	"syscall"
+
+	"github.com/bwmarrin/discordgo"
 )
 
 // Variables used for command line parameters
 var (
-	Token      string
-	TestStatement string
+	Token string
 )
 
 func init() {
-	flag.StringVar(&Token, "t", "", "Bot token")
-	flag.StringVar(&TestStatement, "s", "", "Should be hello world")
-	flag.Parse()
 
-	if Token == "" || TestStatement == "" {
-		flag.Usage()
-		os.Exit(1)
-	}
+	flag.StringVar(&Token, "t", "", "Bot Token")
+	flag.Parse()
 }
 
 func main() {
-	_, err := discordgo.New("Bot " + Token)
+
+	// Create a new Discord session using the provided bot token.
+	dg, err := discordgo.New("Bot " + Token)
 	if err != nil {
 		fmt.Println("error creating Discord session,", err)
 		return
 	}
-	fmt.Println(TestStatement)
+
+	// Register the messageCreate func as a callback for MessageCreate events.
+	dg.AddHandler(messageCreate)
+
+	// In this example, we only care about receiving message events.
+	dg.Identify.Intents = discordgo.IntentsGuildMessages
+
+	// Open a websocket connection to Discord and begin listening.
+	err = dg.Open()
+	if err != nil {
+		fmt.Println("error opening connection,", err)
+		return
+	}
+
+	// Wait here until CTRL-C or other term signal is received.
+	fmt.Println("Bot is now running.  Press CTRL-C to exit.")
+	sc := make(chan os.Signal, 1)
+	signal.Notify(sc, syscall.SIGINT, syscall.SIGTERM, os.Interrupt, os.Kill)
+	<-sc
+
+	// Cleanly close down the Discord session.
+	dg.Close()
+}
+
+// This function will be called (due to AddHandler above) every time a new
+// message is created on any channel that the authenticated bot has access to.
+func messageCreate(s *discordgo.Session, m *discordgo.MessageCreate) {
+
+	// Ignore all messages created by the bot itself
+	// This isn't required in this specific example but it's a good practice.
+	if m.Author.ID == s.State.User.ID {
+		return
+	}
+
+	var channel, _ = s.Channel(m.ChannelID)
+	fmt.Println("received message in channel named: ", channel.Name)
+	fmt.Println("received message content: ", m.Content)
+
+	if strings.HasPrefix(m.Content, ";;fb") {
+		s.ChannelMessageSend(m.ChannelID, strings.TrimPrefix(m.Content, ";;fb"))
+	}
 }
