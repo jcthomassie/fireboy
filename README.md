107cefd1272bada1c87979de12f89eca37597c8a
kso4351 <43012940+kso4351@users.noreply.github.com>
2022-01-24 21:03:06 -0800

    adding custom trigger string feature for dev testing (#5)
    * adding custom trigger string feature for dev testing
    
    * formatting
    
    * more formatting
    
    Co-authored-by: Alan Zhang <kso4351@gmail.com>

--- README.md
+++ README.md
@@ -8,7 +8,14 @@ Add the bot discord token to `fireboy/.env`:
 echo 'DISCORD_TOKEN=**********' > .env
 ```
 
-Run the bot:
+Set trigger word for your test bot in `fireboy/.env` (production default is ;;fb):
+
+```
+# Example using ';;fbat' as the trigger string
+echo 'TRIGGER_STRING=;;fbat' >> .env
+```
+
+Run the bot (builds first and then runs):
 ```
 go run fireboy
 ```
--- main.go
+++ main.go
@@ -50,6 +50,7 @@ func main() {
 
 // This function will be called (due to AddHandler above) every time a new
 // message is created on any channel that the authenticated bot has access to.
+
 func messageCreate(s *discordgo.Session, m *discordgo.MessageCreate) {
 
 	// Ignore all messages created by the bot itself
@@ -57,11 +58,16 @@ func messageCreate(s *discordgo.Session, m *discordgo.MessageCreate) {
 		return
 	}
 
+	triggerstr := os.Getenv("TRIGGER_STRING") // Production uses ;;fb
+	if triggerstr == "" { triggerstr = ";;fb" } // set to prod default
+
+	// Printing logs to console
 	channel, _ := s.Channel(m.ChannelID)
 	fmt.Println("received message in channel named: ", channel.Name)
 	fmt.Println("received message content: ", m.Content)
+	// end log messages
 
-	if strings.HasPrefix(m.Content, ";;fb") {
-		s.ChannelMessageSend(m.ChannelID, strings.TrimPrefix(m.Content, ";;fb"))
+	if strings.HasPrefix(m.Content, triggerstr) {
+		s.ChannelMessageSend(m.ChannelID, strings.TrimPrefix(m.Content, triggerstr))
 	}
 }
